f669712ddb59b96ead3878e3cc2ceb7e09fa39ff
damendozam <damendozam@gmail.com>
2022-01-25 14:42:13 -0500

    Practica 4.1

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function catchForm(e)
+function tableFuncion()
 {
-    e.preventDefault();
-    var name = document.querySelector('#name').value;
-    var date = document.querySelector('#date').value;
-    var msm = document.querySelector('#msm');
-    var queryReturn = '<h1>'+ name +'</h1>';
-    msm.innerHTML = queryReturn;
+    var table = document.querySelector("#tarjet");
+    var array1 = ['A','p','3','!',' ','2','U'];
+    array1 = array1.sort(
+        function()
+        {
+            return 0.5-Math.random();
+        }
+    );
+    table.innerHTML = "";
+    array1.forEach(
+        function(index){
+            var objTar = document.createElement("div");
+            objTar.innerHTML = 
+                '<div class="tarjet">'+ index +'</div>';
+            table.appendChild(objTar);
+        }
+    );
 }
-var sub = document.querySelector('#formHtml');
-sub.addEventListener("submit",catchForm);
+function show()
+{
+    this.classList.add("show");
+}
+
+tableFuncion();
+document.querySelectorAll(".tarjet").forEach(
+    function(index)
+    {
+        index.addEventListener("click",show);
+    }
+);
+
+
+
+
